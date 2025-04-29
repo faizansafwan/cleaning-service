7e5f3c9d3dc0a4ebe687200750be6c27532508fc
faizansafwan <faizansafwan075@gmail.com>
2025-04-29 12:13:32 +0530

    login route created

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { createUser } = require('../controllers/UserController.js');
+const { createUser, loginUser } = require('../controllers/UserController.js');
 
 const router = express.Router();
 
 // register new user
 router.post("/register", createUser);
 
+// login user
+router.post("/login", loginUser);
 module.exports = router;
