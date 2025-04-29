80afb002ae70f2627597858037050bd78a74d4a9
faizansafwan <faizansafwan075@gmail.com>
2025-04-29 12:11:08 +0530

    lgoin page created with jwt

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,9 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User.js');
+
+// fetch the jwt token
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key';
 
 // Create New User
 async function createUser(req, res) {
@@ -10,6 +14,11 @@ async function createUser(req, res) {
   // check if password or username empty
   if (!username || !password) {
     return res.status(400).json({ message: 'Username and password are required' });
+  }
+
+  // password the password length
+  if (password.length < 8) {
+    return res.status(400).json({ message: 'Password must be at least 8 characters long' });
   }
   
   try {
@@ -24,6 +33,7 @@ async function createUser(req, res) {
     const hashedPassword = await bcrypt.hash(password, 10); 
 
 
+    // save new user
     const user = await User.create({
       username,
       password_hash: hashedPassword
@@ -39,4 +49,43 @@ async function createUser(req, res) {
 }
 
 
-module.exports = { createUser }; 
+
+// Login User
+async function loginUser(req, res) {
+
+  // 
+  const { username, password } = req.body;
+
+  // check if username or password empty
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
+  try {
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid username or password' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password_hash);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid password' });
+    }
+
+    // Create JWT token
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      JWT_SECRET,
+      { expiresIn: '6d' }
+    );
+
+
+    // You can generate a token here if needed (e.g., JWT)
+    res.status(200).json({ message: 'Login successful!', user: { id: user.id, username: user.username }, "Token Key": token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong!' });
+  }
+}
+
+module.exports = { createUser, loginUser }; 
